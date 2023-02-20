90ecf1f98f9b65ac9086f2f72d852d7bcaf0a47f
Emre Can Ayar <emrecan.ayar@hotmail.com>
2023-02-20 20:14:13 +0300

    Topic Exchange

---
+++ Exchange Types/RabbitMQ.Topic.Exchange.Consumer/RabbitMQ.Topic.Exchange.Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ozlhavpw:dCQqC5opP19AijsrsJH4o-yJmql2b7pM@hawk.rmq.cloudamqp.com/ozlhavpw");
+
+//Bağlantıyı aktlifleştirme ve kanal açma.
+using IConnection connection = factory.CreateConnection(); //Bağlantı oluşturma
+using IModel channel = connection.CreateModel(); // Oluşturulan bu bağlantı üzerinden bir kanal oluşturduk.
+
+//Publisher daki exchange yapısıyla aynı yapıya sahip bir topic exchange oluşturulacak.
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+//Publisher ile eşlecek olan topic bilgisini kullanıcıdan alalım.
+Console.Write("Dinlenecek Topic Formatını Belirtiniz : ");
+string topic = Console.ReadLine();
+//Channeldan oluşan Queue name i alalım.
+string queueName = channel.QueueDeclare().QueueName;
+
+//Bu aşamadan sonra oluşturduğumuz kuyruk ile exchange bind etmemiz gerekmektedir.
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example",
+    routingKey: topic
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+//Mesajımızı elde edelim
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Exchange Types/RabbitMQ.Topic.Exchange.Publisher/RabbitMQ.Topic.Exchange.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ 
+ Öncelikle Topic Exchange nasıl davranıyordu buna bir girizgah yapalım. 
+
+    Topic Exhange : Routing key'leri kullanarak mesajları kuyruklara yönlendirmek için kullanılan bir exchange dir. Bu exchange ile routing keyin bir kımsına/formatına/yapısına veya yapısındaki keylere göre kuyruklara mesajlar gönderilir.
+
+    Kuyruklar da routing key'e göre bu exchange abone olabilir ve sadece ilgili routing key'e göre gönderilen mesajları alabilirler.
+
+ */
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ozlhavpw:dCQqC5opP19AijsrsJH4o-yJmql2b7pM@hawk.rmq.cloudamqp.com/ozlhavpw");
+
+//Bağlantıyı aktlifleştirme ve kanal açma.
+using IConnection connection = factory.CreateConnection(); //Bağlantı oluşturma
+using IModel channel = connection.CreateModel(); // Oluşturulan bu bağlantı üzerinden bir kanal oluşturduk.
+
+//Öncelikle topic exchange oluşturalım (Tipini Topic olarak ayarladık.)
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+// Daha sonra mesajı göndereceğimizz yapıyı ve kullanıcıdan gelecek olan topic bilgisini alacağımız yapıyı kurgulayalım.
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write("Mesajın Gönderileceği Topic Formatını Bildiriniz : ");
+    string topic = Console.ReadLine(); //Topic bilgisinide aldıktan sonra artık BasicPublish metoduyla mesajımızı gönderebiliriz.
+
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic, //Kullanıcıdan aldığımız topic bilgisini burada routing key olarak belirtiyoruz.
+        body: message
+        );
+}
+
+Console.Read();
